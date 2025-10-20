c6fa742ea8c2c2fcc70b16698db9f896ae70d30c
Sthefanyom <om.sthefany@gmail.com>
2025-10-20 15:28:09 -0300

    Formulario Finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const formulario = document.querySelector('#formulario');
+
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const campoNome = document.querySelector('#name');
+  const errNome = document.querySelector('#errNome');
+  if (campoNome.value.length < 3) {
+    errNome.innerHTML = 'O Nome deve ter no mínimo 3 caracteres.';
+    campoNome.focus();
+    return;
+  } else {
+    errNome.innerHTML = '';
+  }
+
+  const campoEmail = document.querySelector('#email');
+  const errEmail = document.querySelector('#errEmail');
+  if (!campoEmail.value.match(emailRegex)) {
+    errEmail.innerHTML = 'Digite um E-mail válido.';
+    campoEmail.focus();
+    return;
+  } else {
+    errEmail.innerHTML = '';
+  }
+
+  const campoSubject = document.querySelector('#subject');
+  const errSubject = document.querySelector('#errSubject');
+  if (campoSubject.value.length < 5) {
+    errSubject.innerHTML = 'O Assunto deve ter no mínimo 5 caracteres.';
+    campoSubject.focus();
+    return;
+  } else {
+    errSubject.innerHTML = '';
+  }
+
+  const campoMessage = document.querySelector('#message');
+
+  window.alert("Formulário enviado com sucesso!");
+
+  campoNome.value = '';
+  campoEmail.value = '';
+  campoSubject.value = '';
+  campoMessage.value = '';
+});
